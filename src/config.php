288bbbd7405dd25ea454d395c47eb6ca6c67be49
Jesus Valdivia <68979059+jesusvaldivia@users.noreply.github.com>
2022-02-09 15:03:09 -0500

    Update config.php

--- src/config.php
+++ src/config.php
@@ -12,7 +12,7 @@ $CFG->dbuser    = getenv("MYSQL_USERNAME");
 $CFG->dbpass    = getenv("MYSQL_PASSWORD");
 $CFG->prefix    = 'mdl_';
 
-$CFG->wwwroot   = 'https://moodle.bitorical.com';
+$CFG->wwwroot   = getenv("HOST_DOMAIN");
 $CFG->dataroot  = '/var/moodledata';
 $CFG->localcachedir = '/var/localcache';
 $CFG->admin     = 'admin';
@@ -21,6 +21,7 @@ $CFG->sslproxy = true;
 $CFG->directorypermissions = 0777;
 $CFG->alternative_file_system_class = '\tool_objectfs\s3_file_system';
 
+if(getenv("REDIS_SESSION") == "enable"){
 $CFG->session_handler_class = '\core\session\redis';
 $CFG->session_redis_host = getenv("REDIS_ADDRESS");
 $CFG->session_redis_port = 6379;  // Optional.
@@ -28,6 +29,7 @@ $CFG->session_redis_database = 0;  // Optional, default is db 0.
 $CFG->session_redis_prefix = 'mod_us_'; // Optional, default is don't set one.
 $CFG->session_redis_acquire_lock_timeout = 7200;
 $CFG->session_redis_lock_expire = 7200;
+}
 
 require_once(__DIR__ . '/lib/setup.php');
 
