ef170c65303ba4ce8a66697f35e1840051f450be
Jesus Valdivia <68979059+jesusvaldivia@users.noreply.github.com>
2022-02-23 20:15:56 -0500

    enable objectfs

--- src/config.php
+++ src/config.php
@@ -19,7 +19,7 @@ $CFG->admin     = 'admin';
 $CFG->sslproxy = true;
 
 $CFG->directorypermissions = 0777;
-// $CFG->alternative_file_system_class = '\tool_objectfs\s3_file_system';
+$CFG->alternative_file_system_class = '\tool_objectfs\s3_file_system';
 
 if(getenv("REDIS_SESSION") == "enable"){
 $CFG->session_handler_class = '\core\session\redis';
